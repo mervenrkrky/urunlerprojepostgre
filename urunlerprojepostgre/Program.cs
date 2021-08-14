17d43b7db081657c727d300322ecf34412af6e7c
mervenrkrky <burcu90132@gmail.com>
2021-08-14 22:39:55 +0300

    14-08-2021

--- urunlerprojepostgre/Program.cs
+++ urunlerprojepostgre/Program.cs
@@ -17,6 +17,7 @@ namespace urunlerprojepostgre
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmUrun());
+
         }
     }
 }
